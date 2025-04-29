214c934be35637f0fca56caada924a81c3c7b847
aditya-bobate <adityabobate15@gmail.com>
2025-04-29 20:15:56 +0530

    tried add code

--- inputfunc.py
+++ inputfunc.py
@@ -1,2 +1,9 @@
-name = input("Enter Your Name=")
-print("Hello" +name)
+'''name = input("Enter Your Name=")
+print("Hello" +name)'''
+
+x = input("Enter your no")
+y = input("Enter your no")
+x =int(x)
+y = int(y)
+res = x+y
+print("sum is",res)
