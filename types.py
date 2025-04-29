9de456d60fdf2d214f2458dcb574cc5fbbd578ac
aditya-bobate <adityabobate15@gmail.com>
2025-04-29 20:30:30 +0530

    just curious how types() works

---
+++ types.py
@@ -0,0 +1,23 @@
+i = 10
+print(type(i))
+
+f = 11.54
+print(type(f))
+
+c = 3+3j
+print(type(c))
+
+str = "Yoo wassap!!!..."
+print(type(str))
+
+array = [10,20,30]
+print(type(array))
+
+l = (10,20,30)
+print(type(l))
+
+s = {10,20,30}
+print(type(s))
+
+d = {10:"Ten", 20:"Twenty"}
+print(type(d))
