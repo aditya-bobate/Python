ba016881ee662badfcee3ab5e835c724ffaf4b89
aditya-bobate <adityabobate15@gmail.com>
2025-05-02 17:46:15 +0530

    area of circle

--- inputfunc.py
+++ inputfunc.py
@@ -1,9 +1,12 @@
 '''name = input("Enter Your Name=")
 print("Hello" +name)'''
 
-x = input("Enter your no")
+'''x = input("Enter your no")
 y = input("Enter your no")
 x = int(x)
 y = int(y)
 res = x+y
-print("sum is",res)
+print("sum is",res)'''
+r=int(input("Enter Radius of Circle"))
+area = 3.14*r*r
+print("area of cicle :",area)
